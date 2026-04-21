065d1ed39f4001bb752c2fe1034438abcd4bde36
vttrongsu1 <nettrongsu@gmail.com>
2026-04-21 21:14:07 +0700

    Them 776 anh de thi

---
+++ data.js
@@ -0,0 +1,8 @@
+const EXAM_LIST = [
+    {
+        "name": "De_CSD203",
+        "folderDe": "De_CSD203/de",
+        "folderDapan": "De_CSD203/dapan"
+    }
+];
+const LAST_PROGRESS = {"examName": "De_CSD203", "currentIndex": 386};
